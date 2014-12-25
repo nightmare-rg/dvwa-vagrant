054795fcc2c7f8cb965280ef4cb5c5374a17d9d5
T Manson <root@hack.ink>
2014-12-25 11:57:54 -0600

    updated to 14.04 trusty tahir instead of 12.04 precise pangolin

--- Vagrantfile
+++ Vagrantfile
@@ -1,7 +1,7 @@
 Vagrant.configure("2") do |config|
 
-  config.vm.box = "precise64"
-  config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+  config.vm.box = "trusty"
+  config.vm.box_url = "https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box"
 
   config.vm.network :forwarded_port, guest: 80, host: 8088
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@
 # 
 #         USAGE: ./bootstrap.sh 
 # 
-#   DESCRIPTION: 
+#   DESCRIPTION: runs at boot in vagrant to provision server with preinstalled software
 # 
 #       OPTIONS: ---
 #  REQUIREMENTS: ---
@@ -13,7 +13,7 @@
 #         NOTES: ---
 #        AUTHOR: YOUR NAME (), 
 #  ORGANIZATION: 
-#       CREATED: 24.02.2014 09:23
+#       CREATED: 25.12.2014 11:29
 #      REVISION:  ---
 #===============================================================================
 
@@ -26,11 +26,13 @@ sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_aga
 sudo apt-get update
 sudo apt-get -y install mysql-server-5.5 php5-mysql apache2 php5 vim
 
-rm -rf /var/www
-ln -s /vagrant/dvwa-1.0.8 /var/www
+rm -rf /var/www/*
+mkdir -p /var/www
+ln -s /vagrant/dvwa-1.0.8 /var/www/html
 
 mysql -u root -pvmdev < /vagrant/dvwa.sql
 
+mkdir -p /etc/php5/conf.d
 ln -s /vagrant/z_custom.ini /etc/php5/conf.d/z_custom.ini
 
 sudo service apache2 restart
